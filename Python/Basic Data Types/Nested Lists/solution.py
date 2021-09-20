da273b401bfd6bf9be0269344085f59e7ba4de7f
Tointech <leanhthunk@gmail.com>
2021-09-20 19:28:13 +0700

    update explanation

--- Python/Basic Data Types/Nested Lists/solution.py
+++ Python/Basic Data Types/Nested Lists/solution.py
@@ -4,13 +4,15 @@ if __name__ == '__main__':
         name = input()
         score = float(input())
         students_grade.append([name, score])
-    sorted_scores = sorted(list(set([x[1] for x in students_grade])))
+    sorted_scores = sorted(
+        list(set([x[1] for x in students_grade])))  # Sorted list
 
-    second_lowest = sorted_scores[1]
+    second_lowest = sorted_scores[1]  # Put the second lowest score
 
     low_final_list = []
     for student in students_grade:
         if second_lowest == student[1]:
+            # Add other equal second lowest scores
             low_final_list.append(student[0])
     for student in sorted(low_final_list):
         print(student)
